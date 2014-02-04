15bd68e7d0f3ccb80a8c593e2f6f03cdd5795bf0
Nguyễn Tuấn Anh <ubolonton@gmail.com>
2014-02-04 22:26:19 +0700

    fix timeout not being exported from timers

--- impl/timers.js
+++ impl/timers.js
@@ -1,10 +1,10 @@
 var dispatch = require("./dispatch");
 var channels = require("./channels");
 
-function timeout(msecs) {
+exports.timeout = function(msecs) {
   var chan = channels.chan();
   dispatch.queue_delay(function() {
     chan.close();
   }, msecs);
   return chan;
-}
+};
