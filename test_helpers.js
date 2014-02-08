9192d874d44f46a489005c927e3ffbb716ff83c8
Nguyễn Tuấn Anh <ubolonton@gmail.com>
2014-02-08 20:49:16 +0700

    add test helpers

---
+++ test_helpers.js
@@ -0,0 +1,37 @@
+var csp = require("./csp");
+var chan = csp.chan;
+var go = csp.go;
+var put = csp.put;
+var take = csp.take;
+
+var mocha = require("mocha");
+var it = mocha.it;
+
+function identity_chan(x) {
+  var ch = chan(1);
+  go(function*() {
+    yield put(ch, x);
+    ch.close();
+  });
+  return ch;
+}
+
+function g(f) {
+  return function(done) {
+    go(f, [done]);
+  };
+};
+
+function it_(desc, f) {
+  return it(desc, g(function*(done) {
+    var ch = go(f, [], true);
+    yield take(ch);
+    done();
+  }));
+}
+
+module.exports = {
+  identity_chan: identity_chan,
+  go: g,
+  it: it_
+};
