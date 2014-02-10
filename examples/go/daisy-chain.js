21b80e9c6ec03fe8a1fc9a7c54d048774b06b539
Nguyễn Tuấn Anh <ubolonton@gmail.com>
2014-02-10 17:39:35 +0700

    add daisy chain example

---
+++ examples/go/daisy-chain.js
@@ -0,0 +1,28 @@
+// http://talks.golang.org/2012/concurrency.slide#39
+// Daisy-chain
+var csp = require("../../src/csp");
+
+function* chain(left, right) {
+  yield csp.put(left, 1 + (yield csp.take(right)));
+}
+
+csp.go(function*() {
+  var n = 100000;
+  var leftmost = csp.chan();
+  var right = leftmost;
+  var left = leftmost;
+
+  // Start the goroutines
+  for (var i = 0; i < n; i++) {
+    right = csp.chan();
+    csp.go(chain, [left, right]);
+    left = right;
+  }
+
+  // Start the chain
+  csp.go(function*() {
+    yield csp.put(right, 1);
+  });
+
+  console.log((yield csp.take(leftmost)));
+});
