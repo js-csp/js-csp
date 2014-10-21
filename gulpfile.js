52554896311e52f01fb09c1a5e3288ea3e40ba21
Nguyễn Tuấn Anh <ubolonton@gmail.com>
2014-10-22 00:28:51 +0700

    switch from makefile to gulp; add gulp tasks for dev

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+var gulp = require("gulp");
+var mocha = require("gulp-mocha");
+var browserify = require("browserify");
+var source = require('vinyl-source-stream');
+
+gulp.task("dev:test", function() {
+  return gulp.src("test/*.js", {read: false})
+    .pipe(mocha({
+      reporter: "nyan"
+    }));
+});
+
+gulp.task("dev:watch", function() {
+  return gulp.watch(["test/*.js", "src/**/*.js"], ["test:dev"]);
+});
+
+gulp.task("bundle:browser", function() {
+  return browserify({
+  })
+    .require("./src/csp.js", {expose: "csp"})
+    .bundle()
+    .pipe(source("csp.bundled.browser.js"))
+    .pipe(gulp.dest("./build/"));
+});
+
+gulp.task("bundle:node", function() {
+  return browserify({
+    standalone: "csp"
+  })
+    .require("./src/csp.js", {expose: "csp"})
+    .bundle()
+    .pipe(source("csp.bundled.node.js"))
+    .pipe(gulp.dest("./build/"));
+});
+
+// TODO: Test bundles
+gulp.task("test", function() {
+  return gulp.src("test/*.js", {read: false})
+    .pipe(mocha({
+      reporter: "spec"
+    }));
+});
