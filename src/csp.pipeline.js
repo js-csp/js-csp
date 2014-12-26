c9a02601c3a32447a2a58bab2b5e6e0783c59132
zeroware <zero@darksense.com>
2014-12-26 11:30:30 +0100

    ~ 2 space code indent ~ refactor pipeline into pipelineInternal ~ extract taskFn into argument

--- src/csp.pipeline.js
+++ src/csp.pipeline.js
@@ -2,119 +2,116 @@
 
 var csp = require('./csp.core');
 
-function pipeline(n, to, xf, from, close, exHandler, type) {
-    if (n < 0) {
-        throw new Error('n must be positive');
+function pipelineInternal(n, to, xf, from, close, exHandler, taskFn) {
+  if (n < 0) {
+    throw new Error('n must be positive');
+  }
+
+  close = close === undefined ? true : close;
+
+  var jobs = csp.chan(n);
+  var results = csp.chan(n);
+
+  for(var _ = 0; _ < n; _++) {
+
+    csp.go(function* (taskFn, jobs, results) {
+      while (true) {
+        var job = yield csp.take(jobs);
+
+        if (!taskFn(job)) {
+          results.close();
+          break;
+        }
+
+      }
+    }, [taskFn, jobs, results]);
+
+  }
+
+  csp.go(function* (jobs, from, results) {
+    while (true) {
+      var v = yield csp.take(from);
+      if (v === csp.CLOSED) {
+        jobs.close();
+        break;
+      } else {
+        var p = csp.chan(1);
+
+        yield csp.put(jobs, [v, p]);
+        yield csp.put(results, p);
+      }
     }
 
-    close = close === undefined ? true : close;
-    type = type || 'compute';
+  }, [jobs, from, results]);
 
-    var jobs = csp.chan(n);
-    var results = csp.chan(n);
-
-    var process = function(job) {
-        if (job === csp.CLOSED) {
-            results.close();
-
-            return null;
-        } else {
-            var v = job[0];
-            var p = job[1];
-            var res = csp.chan(1, xf, exHandler);
-
-            csp.go(function* (res, v) {
-                yield csp.put(res, v);
-                res.close();
-            }, [res, v]);
-
-            csp.putAsync(p, res);
-
-            return true;
+  csp.go(function* (results, close, to) {
+    while(true) {
+      var p = yield csp.take(results);
+      if (p === csp.CLOSED) {
+        if (close) {
+          to.close();
         }
-    };
-
-    var async = function(job) {
-        if (job === csp.CLOSED) {
-            results.close();
-            return null;
-        } else {
-            var v = job[0];
-            var p = job[1];
-            var res = csp.chan(1);
-            xf(v, res);
-            csp.putAsync(p, res);
-            return true;
-        }
-    };
-
-    for(var _ = 0; _ < n; _++) {
-
-        csp.go(function* (t, jobs, async, process) {
-            while (true) {
-                var job = yield csp.take(jobs);
-
-                if (t === 'async') {
-                    if (!async(job)) {
-                        break;
-                    }
-                } else {
-                    if (!process(job)) {
-                        break;
-                    }
-                }
-            }
-        }, [type, jobs, async, process]);
-
-    }
-
-    csp.go(function* (jobs, from, results) {
-        while (true) {
-            var v = yield csp.take(from);
-            if (v === csp.CLOSED) {
-                jobs.close();
-                break;
-            } else {
-                var p = csp.chan(1);
-
-                yield csp.put(jobs, [v, p]);
-                yield csp.put(results, p);
-            }
-        }
-
-    }, [jobs, from, results]);
-
-    csp.go(function* (results, close, to) {
+        break;
+      } else {
+        var res = yield csp.take(p);
         while(true) {
-            var p = yield csp.take(results);
-            if (p === csp.CLOSED) {
-                if (close) {
-                    to.close();
-                }
-                break;
-            } else {
-                var res = yield csp.take(p);
-                while(true) {
-                    var v = yield csp.take(res);
-                    if (v !== csp.CLOSED) {
-                        yield csp.put(to, v);
-                    } else {
-                        break;
-                    }
-                }
-            }
+          var v = yield csp.take(res);
+          if (v !== csp.CLOSED) {
+            yield csp.put(to, v);
+          } else {
+            break;
+          }
         }
-    }, [results, close, to]);
+      }
+    }
+  }, [results, close, to]);
 
-    return to;
+  return to;
 }
 
+function pipeline(to, af, from, close, exHandler) {
 
-function pipelineAsync(n, to, af, from, close) {
-    return pipeline(n, to, af, from, close, null, 'async');
+  function taskFn(job, results) {
+    if (job === csp.CLOSED) {
+      return null;
+    } else {
+      var v = job[0];
+      var p = job[1];
+      var res = csp.chan(1, af, exHandler);
+
+      csp.go(function* (res, v) {
+        yield csp.put(res, v);
+        res.close();
+      }, [res, v]);
+
+      csp.putAsync(p, res);
+
+      return true;
+    }
+  }
+
+  return pipelineInternal(1, to, af, from, close, exHandler, taskFn);
+}
+
+function pipelineAsync(n, to, af, from, close, exHandler) {
+
+  function taskFn(job) {
+    if (job === csp.CLOSED) {
+      return null;
+    } else {
+      var v = job[0];
+      var p = job[1];
+      var res = csp.chan(1);
+      af(v, res);
+      csp.putAsync(p, res);
+      return true;
+    }
+  }
+
+  return pipelineInternal(n, to, af, from, close, exHandler, taskFn);
 }
 
 module.exports = {
-    pipeline: pipeline,
-    pipelineAsync: pipelineAsync
+  pipeline: pipeline,
+  pipelineAsync: pipelineAsync
 };
