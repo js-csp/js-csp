7b0057783c2ef3959dab77d080856366b6ff2d06
Michael Trotter <spicydonuts@gmail.com>
2015-02-05 00:26:24 -0700

    added goloop

--- src/csp.core.js
+++ src/csp.core.js
@@ -27,6 +27,22 @@ function go(f, args) {
   return spawn(gen, f);
 };
 
+function goloop(f, args) {
+  var recurring;
+  var recur = function() {
+    args = Array.prototype.slice.call(arguments);
+    args.unshift(recur);
+    recurring = true;
+  }
+  recur.apply(null, args);
+  return go(function* () {
+    while (recurring) {
+      recurring = false;
+      yield* f.apply(null, args);
+    }
+  });
+};
+
 function chan(bufferOrNumber, xform, exHandler) {
   var buf;
   if (bufferOrNumber === 0) {
@@ -50,6 +66,7 @@ module.exports = {
 
   spawn: spawn,
   go: go,
+  goloop: goloop,
   chan: chan,
   DEFAULT: select.DEFAULT,
   CLOSED: channels.CLOSED,
--- test/csp.js
+++ test/csp.js
@@ -8,6 +8,7 @@ var a = require("../src/csp.test-helpers"),
 var csp = require("../src/csp"),
     chan = csp.chan,
     go = csp.go,
+    goloop = csp.goloop,
     put = csp.put,
     take = csp.take,
     putAsync = csp.putAsync,
@@ -364,6 +365,27 @@ describe("Goroutine", function() {
     }, [CLOSED]);
     var value = yield take(ch);
     assert.equal(value, CLOSED, "CLOSED is delivered");
+    assert.equal(ch.is_closed(), true, "output channel is closed");
+  });
+});
+
+describe("Goloop", function() {
+  var iterations = 1000;
+  it("should write to channel "+iterations+" times and close it", function*() {
+    var ch = chan();
+    goloop(function*(recur, i) {
+      if (i < iterations) {
+        yield put(ch, i);
+        recur(i+1);
+      } else {
+        ch.close();
+      }
+    }, [0]);
+    var values = [];
+    while ((value = yield take(ch)) !== CLOSED) {
+      values.push(value);
+    }
+    assert.equal(values.length, iterations, "returned 10 values");
     assert.equal(ch.is_closed(), true, "output channel is closed");
   });
 });
