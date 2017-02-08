ed634fd57fdcfd007c7131e6a0bf3960614ebfa9
Ankit Patel <gt3.someday@outlook.com>
2017-02-08 14:39:23 -0600

    eslint cleanup webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,35 @@
-'use strict';
-var es5 = process.env.BABEL_ENV === 'es5'
-var webpack = require('webpack'), join = require('path').join
-var context = __dirname, path = join(context, 'lib')
-var entry = join(context, 'src', 'csp')
-var filename = 'js-csp.js', library = 'csp', libraryTarget = 'umd', umdNamedDefine = true
-var loaders = [
-  { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
-]
-var plugins = []
-var uglify = new webpack.optimize.UglifyJsPlugin({compress: {warnings: false, drop_console: true}})
-if(es5) {
-    plugins.push(uglify)
-    filename = 'js-csp.es5.min.js'
-    path = join(context, 'dist')
+'use script';
+
+const webpack = require('webpack');
+const join = require('path').join;
+
+const es5 = process.env.BABEL_ENV === 'es5';
+
+const context = __dirname;
+const entry = join(context, 'src', 'csp');
+const library = 'csp';
+const libraryTarget = 'umd';
+const umdNamedDefine = true;
+let filename = 'js-csp.js';
+let path = join(context, 'lib');
+
+const loaders = [
+  {
+    test: /\.js$/,
+    exclude: /node_modules/,
+    loader: 'babel-loader',
+  },
+];
+
+const plugins = [];
+const uglify = new webpack.optimize.UglifyJsPlugin({ compress: { warnings: false, drop_console: true } });
+
+if (es5) {
+  plugins.push(uglify);
+  filename = 'js-csp.es5.min.js';
+  path = join(context, 'dist');
 }
-var output = { path, filename, library, libraryTarget, umdNamedDefine }
-module.exports = { context, entry, output, plugins, module: { loaders } }
+
+const output = { path, filename, library, libraryTarget, umdNamedDefine };
+
+module.exports = { context, entry, output, plugins, module: { loaders } };
